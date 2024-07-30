c26e1919a579aed9496b8f080af608685f77b029
DesaiSamiksha07 <155519403+DesaiSamiksha07@users.noreply.github.com>
2024-07-30 22:18:10 +0530

    Add files via upload

---
+++ Task-Project/Default.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="VolleyBall_Final._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div style="height: 542px">
+        <div style="font-size:x-large; text-align: center;">Task-Project</div> 
+        <table class="w-100" style="width: 576px; height: 250px;">
+            <tr>
+                <td colspan="3"></td>
+                <td>users</td>
+                <td colspan="2">product</td>
+                <td>category</td>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <td colspan="2"></td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; userId&nbsp; </td>
+                <td><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox></td>
+                <td colspan="2">
+                    productId&nbsp;&nbsp; <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                </td>
+                <td>CategoryId&nbsp; <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox></td>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <td colspan="2"></td>
+                <td>&nbsp;&nbsp; UserName</td>
+                <td><asp:TextBox ID="TextBox2" runat="server"></asp:TextBox></td>
+                <td colspan="2">
+                    ProductName&nbsp; <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+                </td>
+                <td>CategoryName&nbsp; <asp:TextBox ID="TextBox8" runat="server"></asp:TextBox></td>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <td colspan="2"></td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; address</td>
+                <td><asp:TextBox ID="TextBox3" runat="server"></asp:TextBox></td>
+                <td colspan="2">
+                    description&nbsp; <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+                </td>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <td colspan="3"></td>
+                <td>
+                    <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="Get" Width="75px" />
+                    <asp:Button ID="Button6" runat="server" Text="Reset" Width="75px" OnClick="Button6_Click" />
+                </td>
+                <td colspan="2">
+                    <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click" Width="75px" />
+                    <asp:Button ID="Button2" runat="server" Text="Update" OnClick="Button2_Click" Width="75px" />
+                &nbsp;
+                </td>
+                <td>
+                     <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Delete" Width="75px" />
+                    <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Search" Width="75px" />
+                </td>
+                <td colspan="2"></td>
+            </tr>
+        </table>
+       
+        <asp:GridView ID="GridViewUsers" runat="server" Width="50%" Style="margin-top: 20px; margin: 0 auto;" AutoGenerateColumns="True"></asp:GridView>
+      
+        <asp:GridView ID="GridViewProduct" runat="server" Width="50%" Style="margin: 0 auto; margin-top: 20px;" AutoGenerateColumns="True"></asp:GridView>
+   
+        <asp:GridView ID="GridViewCategory" runat="server" Width="50%" Style="margin: 0 auto; margin-top: 20px;" AutoGenerateColumns="True"></asp:GridView>
+  
+    </div>
+</asp:Content>
---
+++ Task-Project/Default.aspx.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VolleyBall_Final
+{
+    public partial class _Default : Page
+    {
+        private readonly string connectionString = "Data Source=KETAN\\SAMIKSHA_SQLSERV;Initial Catalog=Company_Info;Integrated Security=True;Encrypt=False";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadRecords();
+            }
+        }
+
+        private void LoadRecords()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                
+                using (SqlCommand comm = new SqlCommand("SELECT * FROM users", con))
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter(comm))
+                    {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        GridViewUsers.DataSource = dt;
+                        GridViewUsers.DataBind();
+                    }
+                }
+
+                using (SqlCommand comm = new SqlCommand("SELECT * FROM product", con))
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter(comm))
+                    {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        GridViewProduct.DataSource = dt;
+                        GridViewProduct.DataBind();
+                    }
+                }
+
+                using (SqlCommand comm = new SqlCommand("SELECT * FROM category", con))
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter(comm))
+                    {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        GridViewCategory.DataSource = dt;
+                        GridViewCategory.DataBind();
+                    }
+                }
+                
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                // Insert into users table
+                string usersquery = "INSERT INTO users (UserName, address) VALUES (@UserName, @address)";
+                using (SqlCommand comm = new SqlCommand(usersquery, con))
+                {
+                    comm.Parameters.AddWithValue("@UserName", TextBox2.Text);
+                    comm.Parameters.AddWithValue("@address", TextBox3.Text);
+
+                    comm.ExecuteNonQuery();
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Inserted');", true);
+                }
+
+                // Insert into product table
+                string productquery = "INSERT INTO product (ProductName, description) VALUES (@ProductName, @description)";
+                using (SqlCommand comm = new SqlCommand(productquery, con))
+                {
+                    comm.Parameters.AddWithValue("@ProductName", TextBox5.Text);
+                    comm.Parameters.AddWithValue("@description", TextBox6.Text);
+
+                    comm.ExecuteNonQuery();
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Inserted');", true);
+                }
+
+                // Insert into category table
+                string categoryquery = "INSERT INTO category (CategoryName) VALUES (@CategoryName)";
+                using (SqlCommand comm = new SqlCommand(categoryquery, con))
+                {
+                    comm.Parameters.AddWithValue("@CategoryName", TextBox8.Text);
+
+                    comm.ExecuteNonQuery();
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Inserted');", true);
+                }
+
+                con.Close();
+                LoadRecords();
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                // Update users table
+                string usersquery = "UPDATE users SET UserName = @UserName, address = @address WHERE userId = @userId";
+                using (SqlCommand comm = new SqlCommand(usersquery, con))
+                {
+                    comm.Parameters.AddWithValue("@userId", int.Parse(TextBox1.Text));
+                    comm.Parameters.AddWithValue("@UserName", TextBox2.Text);
+                    comm.Parameters.AddWithValue("@address", TextBox3.Text);
+
+                    comm.ExecuteNonQuery();
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Updated');", true);
+                }
+                
+                // Update product table
+                string productquery = "UPDATE product SET ProductName = @ProductName, description = @description WHERE productId = @productId";
+                using (SqlCommand comm = new SqlCommand(productquery, con))
+                {
+                    comm.Parameters.AddWithValue("@productId", int.Parse(TextBox4.Text));
+                    comm.Parameters.AddWithValue("@ProductName", TextBox5.Text);
+                    comm.Parameters.AddWithValue("@description", TextBox6.Text);
+
+                    comm.ExecuteNonQuery();
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Updated');", true);
+                }
+               
+                // Update category table
+                string categoryquery = "UPDATE category SET CategoryName = @CategoryName WHERE CategoryId = @CategoryId";
+                using (SqlCommand comm = new SqlCommand(categoryquery, con))
+                {
+                    comm.Parameters.AddWithValue("@CategoryId", int.Parse(TextBox7.Text));
+                    comm.Parameters.AddWithValue("@CategoryName", TextBox8.Text);
+
+                    comm.ExecuteNonQuery();
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Updated');", true);
+                }
+
+                con.Close();
+                LoadRecords();
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                // Delete from users table
+                string usersquery = "DELETE FROM users WHERE userId = @userId";
+                using (SqlCommand comm = new SqlCommand(usersquery, con))
+                {
+                    comm.Parameters.AddWithValue("@userId", int.Parse(TextBox1.Text));
+
+                    comm.ExecuteNonQuery();
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Deleted');", true);
+                }
+                
+                // Delete from product table
+                string productquery = "DELETE FROM product WHERE productId = @productId";
+                using (SqlCommand comm = new SqlCommand(productquery, con))
+                {
+                    comm.Parameters.AddWithValue("@productId", int.Parse(TextBox4.Text));
+
+                    comm.ExecuteNonQuery();
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Deleted');", true);
+                }
+                
+                // Delete from category table
+                string categoryquery = "DELETE FROM category WHERE CategoryId = @CategoryId";
+                using (SqlCommand comm = new SqlCommand(categoryquery, con))
+                {
+                    comm.Parameters.AddWithValue("@CategoryId", int.Parse(TextBox7.Text));
+
+                    comm.ExecuteNonQuery();
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Deleted');", true);
+                }
+
+                con.Close();
+                LoadRecords();
+            }
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                // Select from users table
+                string usersquery = "SELECT * FROM users WHERE userId = @userId";
+                using (SqlCommand comm = new SqlCommand(usersquery, con))
+                {
+                    comm.Parameters.AddWithValue("@userId", int.Parse(TextBox1.Text));
+
+                    using (SqlDataReader dr = comm.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            TextBox2.Text = dr["UserName"].ToString();
+                            TextBox3.Text = dr["address"].ToString();
+                        }
+                    }
+                }
+
+                // Select from product table
+                string productquery = "SELECT * FROM product WHERE productId = @productId";
+                using (SqlCommand comm = new SqlCommand(productquery, con))
+                {
+                    comm.Parameters.AddWithValue("@productId", int.Parse(TextBox4.Text));
+
+                    using (SqlDataReader dr = comm.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            TextBox5.Text = dr["ProductName"].ToString();
+                            TextBox6.Text = dr["description"].ToString();
+                        }
+                    }
+                }
+
+                // Select from category table
+                string categoryquery = "SELECT * FROM category WHERE CategoryId = @CategoryId";
+                using (SqlCommand comm = new SqlCommand(categoryquery, con))
+                {
+                    comm.Parameters.AddWithValue("@CategoryId", int.Parse(TextBox7.Text));
+
+                    using (SqlDataReader dr = comm.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            TextBox8.Text = dr["CategoryName"].ToString();
+                        }
+                    }
+                }
+
+                con.Close();
+            }
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            LoadRecords();
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+            TextBox6.Text = "";
+            TextBox7.Text = "";
+            TextBox8.Text = "";
+        }
+    }
+}
